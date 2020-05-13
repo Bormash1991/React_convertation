58dd4f964d8ab00762ad67b7d9ea76b8d4315235
Lutsenko <lutsenko2004@gmail.com>
2020-05-13 10:54:54 +0300

    Updates

---
+++ precache-manifest.ab6c7ca49237c7241024ea0810757cdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04d25173e407434ab2cd00e80052736",
+    "url": "/systebweb/index.html"
+  },
+  {
+    "revision": "943d6e6800b84a6c713c",
+    "url": "/systebweb/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "540e59beaeb5adaf6b18",
+    "url": "/systebweb/static/css/main.ceb96779.chunk.css"
+  },
+  {
+    "revision": "943d6e6800b84a6c713c",
+    "url": "/systebweb/static/js/2.578dcaa8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/systebweb/static/js/2.578dcaa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "540e59beaeb5adaf6b18",
+    "url": "/systebweb/static/js/main.bcbd6879.chunk.js"
+  },
+  {
+    "revision": "d0a597b3617178ecfc55",
+    "url": "/systebweb/static/js/runtime-main.8b9b6c6d.js"
+  }
+]);
